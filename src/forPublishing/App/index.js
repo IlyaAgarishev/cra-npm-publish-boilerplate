7e386346d5b8f967674e5b6dc27f32450e04c92e
IlyaAgarishev <ilyagarishev@gmail.com>
2019-03-27 18:27:21 +0500

    plugin-proposal-class-properties added

--- src/forPublishing/App/index.js
+++ src/forPublishing/App/index.js
@@ -1,6 +1,35 @@
 import React from "react";
 import styles from "./index.module.css";
 
-const App = () => <div className={styles.app}>Hello world 😎 😎 😎</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  clickHandler = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className={styles.button} onClick={this.clickHandler}>
+          Click me
+        </button>
+        {this.state.clicked ? (
+          <div className={styles.text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            beatae sequi officia modi incidunt magnam ducimus molestias? Porro,
+            iste nemo consectetur, unde eos impedit recusandae rerum cupiditate
+            architecto asperiores et!
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./forPublishing";
 
 ReactDOM.render(<App />, document.getElementById("root"));
